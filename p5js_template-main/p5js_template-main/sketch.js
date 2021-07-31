ee1629f7e0221843f954fbe27f599d8f5c9ea8ef
unique-vyshu <74426792+unique-vyshu@users.noreply.github.com>
2021-07-31 23:08:17 +0530

    Add files via upload

---
+++ p5js_template-main/p5js_template-main/sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+  createCanvas(400, 400);
+ var a=10;
+ var b=20;
+ console.log(a+b);
+
+}
+
+function draw() {
+  background("blue");
+
+  text("Hii Hello welcome to the class.!",200,200);
+
+}
